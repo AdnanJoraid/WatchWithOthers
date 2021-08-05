d2181c44ce9466a0ea34525b38cd93a01576dde5
AdnanJoraid <adnanjoraidca@gmail.com>
2021-08-05 00:31:45 -0400

    Added firebase config file and worked on the design of HomeScreen

---
+++ src/database/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyByBQHXoi-_ypGVFdxuyxH97GpmLcSoP70",
+    authDomain: "watchwithothers.firebaseapp.com",
+    projectId: "watchwithothers",
+    storageBucket: "watchwithothers.appspot.com",
+    messagingSenderId: "1045114887573",
+    appId: "1:1045114887573:web:bc7b61675d3028ae6d2a9c"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import {Grid, Button, ButtonGroup, Typography} from '@material-ui/core'
 
 function HomeScreen() {
-    return (
-        <div>
-            Home Screen
-        </div>
-    )
+  return (
+    <Grid container spacing={2} style={{padding: 350}}>
+        <Grid item xs={12} align='center'>
+            <Typography variant='h3' compact='h3'>
+                WatchWithOthers
+            </Typography>
+        </Grid>
+
+        <Grid item xs={12} align='center'>
+            <ButtonGroup disableElevation variant='contained' color='primary'>
+                <Button color="primary">Create a Room</Button>
+                <Button color="secondary">Join a Room</Button>
+            </ButtonGroup>
+        </Grid>
+
+    </Grid>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
