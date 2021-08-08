403e6e8d51f8444a111a31e7822d7964a141196e
AdnanJoraid <adnanjoraidca@gmail.com>
2021-08-07 23:34:41 -0400

    fixed indentations

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
-import { Link, useHistory  } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { db } from "../database/firebase";
 function HomeScreen() {
   const [roomId, setRoomId] = useState("");
   const history = useHistory();
 
   useEffect(() => {
-   setRoomId(roomAutoId())
-  }, [])
+    setRoomId(roomAutoId());
+  }, []);
 
   const roomAutoId = () => {
     //generating a random unique id for roomId
@@ -17,24 +17,24 @@ function HomeScreen() {
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     var charactersLength = characters.length;
     for (var i = 0; i < 6; i++) {
-      result += characters.charAt(
-        Math.floor(Math.random() * charactersLength)
-      );
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result
+    return result;
   };
 
   const onCreateRoomButtonHandler = () => {
-    db.collection('rooms').doc(roomId).set({
-      id: roomId
-    }).then(() => {
-        localStorage.setItem(`isHost-${roomId}`, true)
-        history.push({ pathname: `/room/${roomId}`, state: {roomId}}) 
-    }).catch((e) => console.log(e))
-    
+    db.collection("rooms")
+      .doc(roomId)
+      .set({
+        id: roomId,
+      })
+      .then(() => {
+        localStorage.setItem(`isHost-${roomId}`, true);
+        history.push({ pathname: `/room/${roomId}`, state: { roomId } });
+      })
+      .catch((e) => console.log(e));
   };
 
- 
   return (
     <Grid container spacing={2} className="center">
       <Grid item xs={12} align="center">
@@ -52,7 +52,14 @@ function HomeScreen() {
           >
             Create a Room
           </Button>
-          <Button color="secondary" to='/join' component={Link} style={{color:'white' }}>Join a Room</Button>
+          <Button
+            color="secondary"
+            to="/join"
+            component={Link}
+            style={{ color: "white" }}
+          >
+            Join a Room
+          </Button>
         </ButtonGroup>
       </Grid>
     </Grid>
--- src/screens/Room.js
+++ src/screens/Room.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import '../App.css'
+import "../App.css";
 import { db } from "../database/firebase";
 import { Typography } from "@material-ui/core";
 import youtube from "../api/youtube";
@@ -7,50 +7,48 @@ import VideoList from "./VideoList";
 import Videoplayer from "./VideoPlayer";
 import Search from "./Search";
 
-
 function Room(props) {
   const [videoMetaInfo, setVideoMetaInfo] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState("");
-  const [urlIsDifferent, setUrlIsDifferent] = useState(false)
-  const [currentUrl, setCurrentUrl] = useState('');
+  const [urlIsDifferent, setUrlIsDifferent] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState("");
   const id = props.match.params.id;
   const isRoomHost = localStorage.getItem(`isHost-${id}`);
-  
-  
+
   useEffect(() => {
-    videoUrlDoesExist()
-    db.collection('rooms').doc(id).onSnapshot((doc) => {
-      setCurrentUrl(doc.data().currentVideoUrl)
-      
-    })
-        
-  }, [selectedVideo, urlIsDifferent])
+    videoUrlDoesExist();
+    db.collection("rooms")
+      .doc(id)
+      .onSnapshot((doc) => {
+        setCurrentUrl(doc.data().currentVideoUrl);
+      });
+  }, [selectedVideo, urlIsDifferent]);
 
- 
-
-  const confirmVideo = async() => {
+  const confirmVideo = async () => {
     console.log(selectedVideo);
-    await db.collection('rooms').doc(id).set({
-      id: id, 
-      currentVideoUrl: `${selectedVideo}`, 
-    })
-   
-    videoUrlDoesExist()
-  }
+    await db
+      .collection("rooms")
+      .doc(id)
+      .set({
+        id: id,
+        currentVideoUrl: `${selectedVideo}`,
+      });
+
+    videoUrlDoesExist();
+  };
 
   const videoUrlDoesExist = async () => {
-    const dbDoc = await db.collection('rooms').doc(id);
-    const doc = await dbDoc.get()
-    if (doc.data().currentVideoUrl){
-      if(doc.data().currentVideoUrl !== currentUrl){
-        setCurrentUrl(doc.data().currentVideoUrl)
-        setUrlIsDifferent(true)
-      }else{
-        setUrlIsDifferent(false)
+    const dbDoc = await db.collection("rooms").doc(id);
+    const doc = await dbDoc.get();
+    if (doc.data().currentVideoUrl) {
+      if (doc.data().currentVideoUrl !== currentUrl) {
+        setCurrentUrl(doc.data().currentVideoUrl);
+        setUrlIsDifferent(true);
+      } else {
+        setUrlIsDifferent(false);
       }
     }
-  }
-  
+  };
 
   const onSearch = async (keyword) => {
     await youtube
@@ -62,47 +60,45 @@ function Room(props) {
       .then((response) => {
         setVideoMetaInfo(response.data.items);
         setSelectedVideo(response.data.items[0].id.videoId);
-      })
-   
-
-
+      });
   };
   const onVideoSelected = (videoId) => {
     setSelectedVideo(videoId);
-    confirmVideo()
+    confirmVideo();
   };
 
   if (isRoomHost === "true") {
     return (
-      <div className='App'>
-        
-        <Search onSearch={onSearch}/>
-   
-          <VideoList onVideoSelected={onVideoSelected} data={videoMetaInfo} />
-          <Typography variant='h6'>
-            Click twice on the video you want to select!
-          </Typography>
-        
-          
-    
-        <div className='center' style={{
-        marginLeft: '-285px'
-      }}>
-      
-        <Videoplayer videoId={selectedVideo}/>
+      <div className="App">
+        <Search onSearch={onSearch} />
+
+        <VideoList onVideoSelected={onVideoSelected} data={videoMetaInfo} />
+        <Typography variant="h6">
+          Click twice on the video you want to select!
+        </Typography>
+
+        <div
+          className="center"
+          style={{
+            marginLeft: "-285px",
+          }}
+        >
+          <Videoplayer videoId={selectedVideo} />
         </div>
-       
       </div>
     );
   } else {
-      return (<div style={{
-        margin: 'auto',
-        width: '70%', 
-        padding: '40px'
-      }}>
-
-      <Videoplayer videoId={currentUrl}/>
-      </div>);
+    return (
+      <div
+        style={{
+          margin: "auto",
+          width: "70%",
+          padding: "40px",
+        }}
+      >
+        <Videoplayer videoId={currentUrl} />
+      </div>
+    );
   }
 }
 
