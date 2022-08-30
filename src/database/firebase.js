43b3ab54bd776a11cf2fd0577b3de4b6f470e8b9
Adnan <59744727+AdnanJoraid@users.noreply.github.com>
2022-08-29 20:29:03 -0400

    Update firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 const firebaseConfig = {
-    apiKey: "AIzaSyByBQHXoi-_ypGVFdxuyxH97GpmLcSoP70",
-    authDomain: "watchwithothers.firebaseapp.com",
-    projectId: "watchwithothers",
-    storageBucket: "watchwithothers.appspot.com",
-    messagingSenderId: "1045114887573",
-    appId: "1:1045114887573:web:bc7b61675d3028ae6d2a9c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 firebase.initializeApp(firebaseConfig);
 
